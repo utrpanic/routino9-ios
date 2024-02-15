dbf0520894d97e0c67f79ae28214fc773fdab623
beck <beck22j@gmail.com>
2024-02-15 16:19:50 -0500

    Init the Xcode project.

---
+++ Routine9/ContentView.swift
@@ -0,0 +1,54 @@
+import SwiftData
+import SwiftUI
+
+struct ContentView: View {
+  @Environment(\.modelContext) private var modelContext
+  @Query private var items: [Item]
+
+  var body: some View {
+    NavigationSplitView {
+      List {
+        ForEach(self.items) { item in
+          NavigationLink {
+            Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+          } label: {
+            Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+          }
+        }
+        .onDelete(perform: self.deleteItems)
+      }
+      .toolbar {
+        ToolbarItem(placement: .navigationBarTrailing) {
+          EditButton()
+        }
+        ToolbarItem {
+          Button(action: self.addItem) {
+            Label("Add Item", systemImage: "plus")
+          }
+        }
+      }
+    } detail: {
+      Text("Select an item")
+    }
+  }
+
+  private func addItem() {
+    withAnimation {
+      let newItem = Item(timestamp: Date())
+      self.modelContext.insert(newItem)
+    }
+  }
+
+  private func deleteItems(offsets: IndexSet) {
+    withAnimation {
+      for index in offsets {
+        self.modelContext.delete(self.items[index])
+      }
+    }
+  }
+}
+
+#Preview {
+  ContentView()
+    .modelContainer(for: Item.self, inMemory: true)
+}
